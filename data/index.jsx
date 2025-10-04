1a3527cf86a0edd7b60382a3fec7fab49bcdb221
mermerit <mermerit1996@gmail.com>
2025-10-05 01:36:18 +0300

    w
    w

--- data/index.jsx
+++ data/index.jsx
@@ -1,64 +1,385 @@
 export const products = [
     {
         id: 1,
-        title: "3D Solar System Planets to Explore",
-        des: "Explore the wonders of our solar system with this captivating 3D simulation of the planets using Three.js.",
+        title: " Extra B-One HD50 ",
+        des: "Extra B-One HD50 Malaysia is a monograde diesel engine oil designed to provide " +
+            "strong protection and reliable performance under operating conditions. Suitable for " +
+            "breakaway situations and various commercial vehicles required by SAE 50 viscosity.",
         img: "products/daba-shin.png",
-        mwas:[ "المواصفات الفنية لزيت Extra B1 15W-40:",
+        mwas:[
 
-"درجة اللزوجة: SAE 15W-40",
+"High wear and tear protection in large engines.",
 
-"مؤشر اللزوجة: يصل إلى 125",
+"Excellent viscosity stability under heavy loads and high temperatures.",
 
-"اللزوجة الحركية عند 100°C: حوالي 14.5 مم²/ثانية",
+"Strong resistance to oxidation and sediment formation.\n",
 
-"اللزوجة الحركية عند 40°C: غير محددة",
+"Keeps the engine clean and extends its operating life.",
 
-"رقم القاعدة الكلية (TBN): 4.5 ملغم KOH/غرام",
+"Designed to withstand harsh working conditions in trucks, buses, and industrial equipment.",
 
-"نقطة الوميض: 224°C",
 
-"نقطة التجمّد: -27°C",
-
-"محتوى الرماد الكبريتي: غير محدد"
         ],
+        typical:[
+          "Viscosity grade SAE 50" ,
+          "Kinematic viscosity at 40 °C 200 – 230 cSt" ,
+          "Kinematic viscosity at 100 °C 18.0 – 20.0 cSt" ,
+          "Viscosity index (VI) 95 – 110" ,
+          "Density at 15 °C 0.88 – 0.90 g/cm³" ,
+          "Pour Point -10 to -15 °C" ,
+          "Flash point: 240 – 260 °C" ,
+          "Basal number (TBN) 9 – 12 mg KOH/g" ,
+          "The percentage of sulfur ash is 1.0 - 1.5% by weight"
+        ],
+        standard:[
+            "Accreditations and standards" ,
+            "API CF/CF-2" ,
+            "Compatible with the requirements of many heavy duty diesel engines." ,
+            "Used in commercial vehicles, trucks, buses, and construction equipment."
+        ],
+
+        aplication:[
+
+            "Heavy duty two-stroke and four-stroke diesel engines.",
+            "Large trucks and buses.",
+            "Agricultural and construction equipment.",
+            "Suitable for working in hot climates and continuous load conditions."
+        ],
+        Packing:[
+            "1 litre",
+            "4 litres",
+            "5 litres",
+            "Drums of 200 litres"
+        ],
+
+
         // iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/fm.svg"],
         // link: "/ui.earth.com",
         category:"oil"
     },
+
     {
         id: 2,
-        title:"(Extra B1) 15W-30",
-        des: "هو زيت محرك متعدد الدرجات، يُصنّع في ماليزيا، ويُستخدم بشكل شائع في محركات الديزل والبنزين الثقيلة. يتميّز هذا الزيت بتركيبته التي توفر حماية فعّالة للمحرك في ظروف التشغيل القاسية",
+        title: "Extra B-One 5W-20",
+        des: "Malaysian Extra B-One 5W-20 is an advanced, high-tech motor oil, specially formulated to provide maximum engine protection" +
+            " while reducing friction and improving fuel efficiency. It is suitable for the latest generation of modern engines" +
+            ", including gasoline and hybrid engines equipped with direct injection (GDI) or turbocharged systems.",
+        img: "products/daba-zard.png",
+        mwas:["Excellent cold-start flow to reduce wear.",
+        "High viscosity stability at high temperatures.",
+        "Reduces deposit and sludge formation.",
+        "Improved fuel economy.",
+        "Strong oxidation resistance and extended oil life.",
+        "Effective engine protection under severe city and highway driving conditions."],
+        typical:["Viscosity Grade SAE 5W-30",
+            "Kinematic Viscosity at 40°C: 60 – 70 cSt",
+            "Kinematic Viscosity at 100°C: 10.5 – 11.5 cSt",
+            "Viscosity Index (VI): 160 – 170",
+            "HTHS Viscosity at 150°C: 3.0 – 3.5 mPa·s",
+            "Density at 15°C: 0.84 – 0.86 g/cm³",
+            "Pour Point: -35 to -45°C",
+            "Flash Point: 220 – 235°C",
+            "Base Number (TBN): 8 – 10 mg KOH/g",
+            "Sulfated Ash Content: 0.7 – 1.0 wt%"],
+
+        standard:[
+           "API SP / SN Plus / SN",
+           "ILSAC GF-6A (Compliant with Modern Requirements)",
+           "Suitable for specifications of some international manufacturers such as Ford, Chrysler, Honda, Toyota, and others."
+        ],
+
+        aplication:[
+
+         "Modern gasoline cars (naturally aspirated and turbocharged).",
+         "GDI (gasoline direct injection) engines." ,
+         "Hybrid cars.",
+         "Ideal for daily driving in busy cities and long journeys."
+        ],
+        Packing:[
+            "1 litre",
+            "4 litres",
+            "5 litres",
+
+        ],
+
+        category:"oil"
+    },
+    {
+        id: 3,
+        title:"(Extra B1) 5W-30",
+        des: "Extra B-One 5W-30 from Malaysia is a premium motor oil designed with advanced technologies " +
+            "to provide superior engine protection under all driving conditions. " +
+            "It combines excellent performance, fuel economy, and extended engine life.",
+
         img: "products/daba-rasasi.png",
         // iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/stream.svg", "/c.svg"],
         // link: "/ui.yoom.com",
-        mwas:[],
-        category:"زيت فرامل"
+        mwas:["Advanced protection against wear, rust, and deposits." ,
+            "Excellent low-temperature performance (smooth cold start).",
+            "High viscosity stability at high temperatures." ,
+            "Reduces friction, improving fuel economy.",
+            "High oxidation resistance and extends oil life.",
+            "Compatible with the latest engine technologies (gasoline and hybrid)."],
 
+        typical:["Viscosity Grade SAE 5W-30",
+            "Kinematic Viscosity at 40°C: 60 – 70 cSt",
+            "Kinematic Viscosity at 100°C: 10.5 – 11.5 cSt",
+            "Viscosity Index (VI): 160 – 170",
+            "HTHS Viscosity at 150°C: 3.0 – 3.5 mPa·s",
+            "Density at 15°C: 0.84 – 0.86 g/cm³",
+            "Pour Point: -35 to -45°C",
+            "Flash Point: 220 – 235°C",
+            "Base Number (TBN): 8 – 10 mg KOH/g",
+            "Sulfated Ash Content: 0.7 – 1.0 wt%"],
 
-    },
+        standard:[
+            "API SP / SN Plus / SN",
+            "ILSAC GF-6A",
+            "Compliant with the catalog requirements of many international companies"
+        ],
 
-    {
-        id: 3,
-        title: "3D Solar System Planets to Explore",
-        des: "Explore the wonders of our solar system with this captivating 3D simulation of the planets using Three.js.",
-        img: "products/daba-zard.png",
-        // iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/fm.svg"],
-        // link: "/ui.earth.com",
-        mwas:[],
+        aplication:[
+
+            "Modern gasoline cars (naturally aspirated and turbocharged).",
+            "Cars equipped with direct injection (GDI) systems.",
+            "Hybrid cars.",
+            "City driving (Stop & Go) and long highway trips."
+        ],
+        Packing:[
+            "1 litre",
+            "4 litres",
+            "5 litres",
+
+        ],
+
         category:"oil"
+
+
     },
     {
         id: 4,
-        title:"باشترین شت",
-        des: "Simplify your video conferencing experience with Yoom. Seamlessly connect with colleagues and friends.",
+        title:"Extra B-One 10W-40",
+        des: "Extra B-One 10W-40 is a multi-grade motor oil formulated with advanced technology, " +
+            "offering the perfect combination of protection and friction. It boasts excellent wear and oxidation resistance," +
+            " making it an ideal choice for modern engines used in harsh operating conditions.",
         img: "products/daba-sawz.png",
         // iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/stream.svg", "/c.svg"],
         // link: "/ui.yoom.com",
-        mwas:[],
+        mwas:["Strong engine protection at high temperatures." ,
+        "Excellent viscosity stability prevents thermal breakdown." ,
+        "High oxidation resistance and extended oil life." ,
+        "Reduces friction, contributing to improved fuel economy." ,
+        "Prevents the formation of deposits and sludge." ,
+        "Suitable for long journeys and severe driving conditions."],
+
+        typical:[
+        "Viscosity Grade SAE 10W-40",
+        "Kinematic Viscosity at 40°C: 85 – 95 cSt",
+        "Kinematic Viscosity at 100°C: 13.5 – 15.0 cSt" ,
+        "Viscosity Index (VI): 150 – 165",
+        "HTHS Viscosity at 150°C: 3.5 – 3.8 mPa·s",
+        "Density at 15°C: 0.85 – 0.88 g/cm³",
+        "Pour Point: -25 to -35°C",
+        "Flash Point: 225 – 240°C" ,
+        "Base Number (TBN): 8 – 10 mg KOH/g",
+        "Sulfated Ash Content: 0.8 – 1.2 wt%"
+        ],
+        standard:[
+          "API SP / SN / SM" ,
+          "ACEA A3/B4 (depending on some versions)" ,
+          "Compatible with the specifications of many European and Asian car manufacturers."
+        ],
+
+        aplication:[
+            "Recommended applications",
+            "Heavy duty two-stroke and four-stroke diesel engines.",
+            "Large trucks and buses.",
+            "Agricultural and construction equipment.",
+            "Suitable for working in hot climates and continuous load conditions."
+        ],
+        Packing:[
+            "1 litre",
+            "4 litres",
+            "5 litres",
+            "Drums of 200 litres"
+        ],
         category:"oil"
 
 
-    }
-    ];
+    },
+    {
+        id: 5,
+        title:"Extra B-One 10W-40",
+        des: "Extra B-One 10W-40 is a multi-grade motor oil formulated with advanced technology, " +
+            "offering the perfect combination of protection and friction. It boasts excellent wear and oxidation resistance," +
+            " making it an ideal choice for modern engines used in harsh operating conditions.",
+        img: "products/daba-pink.png",
+        // iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/stream.svg", "/c.svg"],
+        // link: "/ui.yoom.com",
+        mwas:["Strong engine protection at high temperatures." ,
+            "Excellent viscosity stability prevents thermal breakdown." ,
+            "High oxidation resistance and extended oil life." ,
+            "Reduces friction, contributing to improved fuel economy." ,
+            "Prevents the formation of deposits and sludge." ,
+            "Suitable for long journeys and severe driving conditions."],
+
+        typical:[
+            "Viscosity Grade SAE 10W-40",
+            "Kinematic Viscosity at 40°C: 85 – 95 cSt",
+            "Kinematic Viscosity at 100°C: 13.5 – 15.0 cSt" ,
+            "Viscosity Index (VI): 150 – 165",
+            "HTHS Viscosity at 150°C: 3.5 – 3.8 mPa·s",
+            "Density at 15°C: 0.85 – 0.88 g/cm³",
+            "Pour Point: -25 to -35°C",
+            "Flash Point: 225 – 240°C" ,
+            "Base Number (TBN): 8 – 10 mg KOH/g",
+            "Sulfated Ash Content: 0.8 – 1.2 wt%"
+        ],
+        standard:[
+            "API SP / SN / SM" ,
+            "ACEA A3/B4 (depending on some versions)" ,
+            "Compatible with the specifications of many European and Asian car manufacturers."
+        ],
+
+        aplication:[
+            "Recommended applications",
+            "Heavy duty two-stroke and four-stroke diesel engines.",
+            "Large trucks and buses.",
+            "Agricultural and construction equipment.",
+            "Suitable for working in hot climates and continuous load conditions."
+        ],
+        Packing:[
+            "1 litre",
+            "4 litres",
+            "5 litres",
+            "Drums of 200 litres"
+        ],
+        category:"oil"
+
+
+    },
+    {
+        id: 6,
+        title:"DOT 4 Brake Fluid",
+        des: "DOT 4 brake fluid is a hydraulic fluid used in vehicle brake and clutch systems. " +
+            "It has important specifications that make it suitable for brake systems that require high thermal resistance " +
+            " and reliable performance.",
+        img: "products/zait-faraml.png",
+        // iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/stream.svg", "/c.svg"],
+        // link: "/ui.yoom.com",
+        mwas:["High boiling point resistance: DOT 4 has a higher tolerance to heat than DOT 3, making it better for frequent braking or high-heat conditions.",
+        "Moisture absorption: Being a glycol oil, DOT 4 absorbs moisture from the atmosphere over time, which alters its properties and lowers its wet boiling point. Therefore, it must be replaced periodically.",
+        "Corrosion inhibitor content: Contains additives that reduce metal corrosion (pipes, cylinders, calipers, etc.) and maintain the integrity of metal parts.",
+        "Compatibility: It is often compatible with DOT 3 and DOT 4 fluids (i.e., they can be mixed if necessary), but should not be mixed with silicone-based oils such as DOT 5."],
+
+        typical:["The primary component is glycol ether and/or glycol ether with additives such as borate esters and corrosion inhibitors.",
+        "The dry boiling point is approximately 230°C for DOT 4 oil.",
+        "The wet boiling point is approximately 155°C when the oil has absorbed some moisture." ,
+        "The standard specifications are in accordance with standards such as FMVSS 116 DOT 4, SAE J1704, and ISO 4925 – Category 4.",
+        "Viscosity: The oil viscosity must be suitable for low-temperature operation to ensure the system operates even in cold weather;",
+        " some DOT 4 “LV” (low viscosity) grades are specifically designed for this purpose."],
+        standard:[
+            "API SP / SN / SM" ,
+            "ACEA A3/B4 (depending on some versions)" ,
+            "Compatible with the specifications of many European and Asian car manufacturers."
+        ],
+
+        aplication:[
+          "If your vehicle uses DOT 4, do not downgrade to DOT 3 unless specifications permit it, as the lower boiling point could compromise safety.",
+          "The oil should be changed periodically according to the manufacturer's recommendations or when signs of deterioration appear, such as darkening, foaming, or a \"soft\" or spongy brake pedal feel.",
+          "Prevent oil from absorbing moisture after opening: Keep the tank cap tightly closed and use sealed containers."
+        ],
+        Packing:[
+        ],
+        category:"bike oil"
+
+    },
+    {
+        id: 7,
+        title:"Extra B-One 10W-30",
+        des: "Extra B1 10W-30 is a multigrade motor oil manufactured in Malaysia and commonly used in heavy-duty diesel and gasoline engines. " +
+            "This oil features a formulation that provides effective engine protection under severe operating conditions.",
+        img:"products/tank-30.png",
+        mwas:["This oil is recommended for use in heavy-duty diesel and gasoline engines operating under harsh operating conditions, such as trucks, buses, and heavy equipment. Always refer to your owner's manual or consult a qualified technician to ensure the oil meets your engine's specific requirements."],
+
+        typical:["Viscosity grade: 10W-30, meaning the oil is liquid when cold (10W) and has a viscosity of 30 when the engine is running." ,
+        "Approved specifications (API / ACEA / OEM approvals): For example: API CJ-4, CI-4, CH-4 or equivalent, and sometimes ACEA (if intended for European markets).",
+        "Total Base Number (TBN): A number sufficient to neutralize the acidity produced by fuel combustion (the higher the number, the better in harsh environments).",
+        "Oxidation Stability: The oil must be resistant to thermal decomposition and oxidation at high temperatures.",
+        "Anti-wear / EP additives: Additives that protect metal surfaces from wear.",
+        "Detergent / Dispersant / Anti-foam additives: To keep the engine clean and prevent deposits.",
+        "Pour Point: To determine the lowest temperature at which the oil can flow.",
+        "Flash Point: A measure of resistance. High-temperature ignition",
+        "Density at 15°C to determine density properties (usually given in kg/L)"
+        ],
+        standard:[
+
+        ],
+
+        aplication:[
+
+        ],
+        Packing:[
+
+        ],
+        category:"cooler oil"
+
+
+    },
+    {
+        id: 8,
+        title:"Extra B-One 15W-40",
+        des: "Extra B1 15W-40 is a multigrade motor oil manufactured in Malaysia and commonly used in heavy-duty diesel and gasoline engines. " +
+            "This oil features a formulation that provides effective engine protection under severe operating conditions.",
+        img:"products/tank-40.png",
+        mwas:["This oil is recommended for use in heavy-duty diesel and gasoline engines operating under harsh operating conditions, such as trucks, buses, and heavy equipment. Always refer to your owner's manual or consult a qualified technician to ensure the oil meets your engine's specific requirements."],
+
+        typical:["Viscosity grade: SAE 15W-40",
+            "Viscosity index: Up to 125",
+            "Kinematic viscosity at 100°C: Approximately 14.5 mm²/s",
+            "Kinematic viscosity at 40°C: Not specified",
+            "Total base number (TBN): 4.5 mg KOH/g",
+            "Flash point: 224°C",
+            "Freezing point: -27°"
+        ],
+        standard:[
+
+        ],
+
+        aplication:[
+
+        ],
+        Packing:[
+
+        ],
+        category:"cooler oil"
+
+
+    },
+    {
+        id: 9,
+        title:"Extra B-One 0W-1G",
+        des: "Extra P One Malaysia is currently in the process of developing its products specifically for hybrid vehicles. At the same time, the company is implementing overall improvements and updates to its product line, aiming to enhance performance, quality, and innovation, in order to stand out more prominently among global oil companies.",
+        img:"products/zait-gas.png",
+        mwas:[],
+
+        typical:[
+        ],
+        standard:[
+
+        ],
+
+        aplication:[
+
+        ],
+        Packing:[
+
+        ],
+        category:"diesel oil"
+
+
+    },
+
+
+
+];
